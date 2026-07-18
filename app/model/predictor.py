5f6b9c6cd8d97a6bf2eb44b23ed1a3cd6df8021c
Manabendu-ai <rikugithub@gmail.com>
2026-07-18 13:14:40 +0530

    Updated Mai

--- app/model/predictor.py
+++ app/model/predictor.py
@@ -1,48 +1,34 @@
 import numpy as np
-from PIL import Image
 
 from app.model.loader import model, CLASS_NAMES
+from app.utils import preprocess_image
 
 
 class Predictor:
+
     def __init__(self):
         self.model = model
         self.class_names = CLASS_NAMES
 
-    def preprocess(self, image: Image.Image):
-        """
-        Preprocess the uploaded image before inference.
-        """
+    def predict(self, image_bytes: bytes):
 
-        image = image.convert("RGB")
-        image = image.resize((128, 128))
-
-        image = np.array(image, dtype=np.float32)
-        image = image / 255.0
-
-        image = np.expand_dims(image, axis=0)
-
-        return image
-
-    def predict(self, image: Image.Image):
-
-        image = self.preprocess(image)
+        image = preprocess_image(image_bytes)
 
         predictions = self.model.predict(image, verbose=0)[0]
 
-        predicted_index = np.argmax(predictions)
+        predicted_index = int(np.argmax(predictions))
 
         predicted_class = self.class_names[predicted_index]
 
         confidence = float(predictions[predicted_index] * 100)
 
         probabilities = {
-            self.class_names[i]: float(predictions[i] * 100)
+            self.class_names[i]: round(float(predictions[i] * 100), 2)
             for i in range(len(self.class_names))
         }
 
         return {
             "prediction": predicted_class,
             "confidence": round(confidence, 2),
-            "probabilities": probabilities
+            "probabilities": probabilities,
         }
