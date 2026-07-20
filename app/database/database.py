8992ccd91d462bb99e9bb30320bc3911d7db832f
Manabendu-ai <rikugithub@gmail.com>
2026-07-20 20:14:09 +0530

    updated database.py

--- app/database/database.py
+++ app/database/database.py
@@ -1,9 +1,19 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_PORT = os.getenv("MYSQL_PORT")
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
 
 DATABASE_URL = (
-    "mysql+pymysql://riku:YOUR_PASSWORD@localhost/monco"
+    f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}"
+    f"@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
 )
 
 engine = create_engine(
